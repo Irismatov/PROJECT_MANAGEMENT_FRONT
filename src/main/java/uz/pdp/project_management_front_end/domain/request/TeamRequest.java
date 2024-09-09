02386b53714b22f9d6815641a5f46946129f8729
SardorBoboyev <boboyevsardor350@gmail.com>
2024-09-09 14:14:12 +0500

    All blank repos, service and req res enum views are written

--- src/main/java/uz/pdp/project_management_front_end/domain/request/TeamRequest.java
+++ src/main/java/uz/pdp/project_management_front_end/domain/request/TeamRequest.java
@@ -1,6 +1,5 @@
 package uz.pdp.project_management_front_end.domain.request;
 
-import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;

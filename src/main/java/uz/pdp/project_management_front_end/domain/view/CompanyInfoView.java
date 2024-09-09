f4bfc5cb0aa0b619eb2ea3f9ba93e2a35b9c6038
SardorBoboyev <boboyevsardor350@gmail.com>
2024-09-09 15:41:18 +0500

    company full prepared;

--- src/main/java/uz/pdp/project_management_front_end/domain/view/CompanyInfoView.java
+++ src/main/java/uz/pdp/project_management_front_end/domain/view/CompanyInfoView.java
@@ -1,4 +1,22 @@
 package uz.pdp.project_management_front_end.domain.view;
 
-public interface CompanyInfoView {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class CompanyInfoView {
+    private UUID id;
+    private String name;
+    private String address;
+    private LocalDate establishmentDate;
+    boolean isBlocked;
+    private String ceoUsername;
 }

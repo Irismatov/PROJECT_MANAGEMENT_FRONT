cb18432d67dcea03c005140026c62d4ad8362653
Irismatov <irismatoffj@mail.ru>
2024-09-09 09:37:19 +0500

    first commit

---
+++ src/main/java/uz/pdp/project_management_front_end/ProjectManagementFrontEndApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.project_management_front_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementFrontEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectManagementFrontEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/pdp/project_management_front_end/ProjectManagementFrontEndApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.project_management_front_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementFrontEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

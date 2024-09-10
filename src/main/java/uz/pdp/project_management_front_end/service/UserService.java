41cc45a21459b5e9c9beedff98e2918d41f523b4
soh1b003 <sohibjinnorboboyev@gmail.com>
2024-09-10 22:52:34 +0500

    product CRUD update

--- src/main/java/uz/pdp/project_management_front_end/service/UserService.java
+++ src/main/java/uz/pdp/project_management_front_end/service/UserService.java
@@ -67,4 +67,19 @@ public class UserService {
         );
         return response.getBody();
     }
+
+
+    public List<UserResponse> getAllProductOwnersAndProductIsNull(){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(httpSession.getAttribute("token").toString());
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+        ResponseEntity<List<UserResponse>> response = restTemplate.exchange(
+                "http://localhost:8080/users/getAllProductOwnersAndProductIsNull",
+                HttpMethod.GET,
+                entity,
+                new ParameterizedTypeReference<>() {}
+        );
+        return response.getBody();
+    }
 }

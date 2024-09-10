1ef9d81b74a64dae0bb5437ab86b11cb4a0a8574
soh1b003 <sohibjinnorboboyev@gmail.com>
2024-09-10 10:38:50 +0500

    product CRUD home

--- src/main/java/uz/pdp/project_management_front_end/service/ProductService.java
+++ src/main/java/uz/pdp/project_management_front_end/service/ProductService.java
@@ -4,8 +4,10 @@ package uz.pdp.project_management_front_end.service;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -13,6 +15,7 @@ import uz.pdp.project_management_front_end.domain.request.ProductRequest;
 import uz.pdp.project_management_front_end.domain.view.ProductInfoView;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class ProductService {
@@ -42,6 +45,39 @@ public class ProductService {
         HttpHeaders headers = new HttpHeaders();
         headers.setBearerAuth(httpSession.getAttribute("token").toString());
         HttpEntity<ProductRequest> entity = new HttpEntity<>(null, headers);
+        ResponseEntity<List<ProductInfoView>> entity1 = restTemplate.exchange(
+                "http://localhost:8080/product",
+                HttpMethod.GET,
+                entity,
+                new ParameterizedTypeReference<>() {
+                }
+        );
+        return entity1.getBody();
+    }
 
+
+    public void delete(UUID id) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(httpSession.getAttribute("token").toString());
+        HttpEntity<ProductRequest> entity = new HttpEntity<>(null, headers);
+        restTemplate.delete(
+                "http://localhost:8080/product",
+                HttpMethod.DELETE,
+                entity,
+                new ParameterizedTypeReference<>() {}
+        );
+    }
+
+
+    public void update(UUID id, ProductRequest productRequest) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(httpSession.getAttribute("token").toString());
+        HttpEntity<ProductRequest> entity = new HttpEntity<>(productRequest, headers);
+        restTemplate.put(
+                "http://localhost:8080/product",
+                HttpMethod.PUT,
+                entity,
+                new ParameterizedTypeReference<>() {}
+        );
     }
 }
